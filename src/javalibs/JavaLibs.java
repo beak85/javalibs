74cb7e47c138a7538455802f9269805b79efb5a5
Matt Geiger <mrgeiger@mrgeiger>
2022-08-28 09:43:07 -0400

    init

---
+++ src/javalibs/JavaLibs.java
@@ -0,0 +1,16 @@
+package javalibs;
+
+/**
+ *
+ * @author 
+ */
+public class JavaLibs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
